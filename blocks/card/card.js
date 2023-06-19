a5317ef7a417a7be5a246e93e63473e4f75cb4c9
Tania Mathern <mathern@adobe.com>
2023-06-19 15:37:57 -0700

    Initial commit

--- blocks/card/card.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-    return `
-        <mj-section mj-class="mj-card"  padding-left="70px" padding-right="70px" background-color="#FFFFFF"   >
-            <mj-column width="100%" align="left" mj-class="mj-card">
-            <mj-text>${block.innerHTML}</mj-text>
-            </mj-column>   
-        </mj-section>
-      `;
-  }
