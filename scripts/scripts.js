ca9960e460d3d10099f6c7a590667052958edea3
Tania Mathern <mathern@adobe.com>
2023-06-19 15:40:15 -0700

    Import code

---
+++ scripts/scripts.js
@@ -0,0 +1,62 @@
+import {
+  sampleRUM,
+  waitForLCP,
+  decorateBlock,
+  buildBlock,
+} from './lib-franklin.js';
+import { decorateMain, toMjml, init } from './functions.js';
+
+window.hlx = window.hlx || {};
+window.hlx.RUM_GENERATION = 'hlx-email'; // add your RUM generation information here
+
+/**
+ * loads everything needed to get to LCP.
+ */
+async function loadEager(doc) {
+  const main = doc.querySelector('main');
+
+  const footer = doc.querySelector('footer');
+
+  const footerDiv = document.createElement('div');
+  main.append(footerDiv);
+
+  const footerBlock = buildBlock('footer', '');
+  footerDiv.append(footerBlock);
+
+  decorateMain(main);
+  await waitForLCP([]);
+
+  const html = await toMjml(main, footer);
+  let frame = document.getElementById('__emailFrame');
+  if (!frame) {
+    frame = document.createElement('iframe');
+    frame.srcdoc = html;
+    frame.width = '100%';
+    frame.height = '100%';
+    frame.id = '__emailFrame';
+    document.body.insertAdjacentElement('beforeend', frame);
+  } else {
+    frame.srcdoc = html;
+  }
+
+  if (document.querySelector('helix-sidekick')) {
+    await import('../tools/sidekick/plugins.js');
+  } else {
+    document.addEventListener('helix-sidekick-ready', () => import('../tools/sidekick/plugins.js'), { once: true });
+  }
+}
+
+/**
+ * loads everything that doesn't need to be delayed.
+ */
+async function loadLazy(/* doc */) {
+  sampleRUM('lazy');
+}
+
+async function loadPage() {
+  init(window);
+  await loadEager(document);
+  await loadLazy(document);
+}
+
+loadPage();
